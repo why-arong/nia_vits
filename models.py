665d0d9cd3815177da0139994c09e11dc029aadd
pilmo kim <68311908+why-arong@users.noreply.github.com>
2025-04-21 21:27:30 +0900

    Update models.py
    Deleting it in order to remove the 'rand_like' node.

--- models.py
+++ models.py
@@ -517,7 +517,7 @@ class SynthesizerTrn(nn.Module):
     m_p = torch.matmul(attn.squeeze(1), m_p.transpose(1, 2)).transpose(1, 2) # [b, t', t], [b, t, d] -> [b, d, t']
     logs_p = torch.matmul(attn.squeeze(1), logs_p.transpose(1, 2)).transpose(1, 2) # [b, t', t], [b, t, d] -> [b, d, t']
 
-    z_p = m_p + torch.randn_like(m_p) * torch.exp(logs_p) * noise_scale
+    z_p = m_p
     z = self.flow(z_p, y_mask, g=g, reverse=True)
     o = self.dec((z * y_mask)[:,:,:max_len], g=g)
     return o, attn, y_mask, (z, z_p, m_p, logs_p)
