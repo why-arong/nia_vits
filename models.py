b09700d23bda95637d3d67e6cdf958ea61378c2a
why-arong <pilmokim99@gmail.com>
2025-05-06 21:34:21 +0900

    fds

--- models.py
+++ models.py
@@ -164,12 +164,12 @@ class TextEncoder(nn.Module):
       p_dropout)
     self.proj= nn.Conv1d(hidden_channels, out_channels * 2, 1)
 
-  def forward(self, text, text_lengths):
-    x = self.emb(text) * math.sqrt(self.hidden_channels) # [b, t, h]
-    x = torch.transpose(x, 1, -1) # [b, h, t]
-    x_mask = torch.unsqueeze(commons.sequence_mask(text_lengths, x.size(2)), 1).to(x.dtype)
+  def forward(self, enc_text, enc_text_lengths):
+    x = self.emb(enc_text) * math.sqrt(self.hidden_channels) # [b, t, h]
+    transposed_x = torch.transpose(x, 1, -1) # [b, h, t]
+    x_mask = torch.unsqueeze(commons.sequence_mask(enc_text_lengths, transposed_x.size(2)), 1).to(x.dtype)
 
-    x = self.encoder(x * x_mask, x_mask)
+    x = self.encoder(transposed_x * x_mask, x_mask)
     stats = self.proj(x) * x_mask
 
     m, logs = torch.split(stats, self.out_channels, dim=1)
