62dbb03328bcdc28f3c650bb8563e98d968a78e3
why-arong <pilmokim99@gmail.com>
2025-05-06 12:28:34 +0900

    fds

--- models.py
+++ models.py
@@ -507,20 +507,23 @@ class SynthesizerTrn(nn.Module):
       logw = self.dp(x, x_mask, g=g, reverse=True, noise_scale=noise_scale_w)
     else:
       logw = self.dp(x, x_mask, g=g)
+    print(f"logw: {logw}")
     w = torch.exp(logw) * x_mask * length_scale
     w_ceil = torch.ceil(w)
     y_lengths = torch.clamp_min(torch.sum(w_ceil, [1, 2]), 1).long()
     y_mask = torch.unsqueeze(commons.sequence_mask(y_lengths, None), 1).to(x_mask.dtype)
     attn_mask = torch.unsqueeze(x_mask, 2) * torch.unsqueeze(y_mask, -1)
     attn = commons.generate_path(w_ceil, attn_mask)
-
+    print(f"attn: {attn}")
     m_p = torch.matmul(attn.squeeze(1), m_p.transpose(1, 2)).transpose(1, 2) # [b, t', t], [b, t, d] -> [b, d, t']
     logs_p = torch.matmul(attn.squeeze(1), logs_p.transpose(1, 2)).transpose(1, 2) # [b, t', t], [b, t, d] -> [b, d, t']
 
     # z_p = m_p + torch.randn_like(m_p) * torch.exp(logs_p) * noise_scale
     z_p = m_p
     z = self.flow(z_p, y_mask, g=g, reverse=True)
+    print(f"z: {z}")
     o = self.dec((z * y_mask)[:,:,:max_len], g=g)
+    print(f"o: {o}")
     return o, attn, y_mask, (z, z_p, m_p, logs_p)
 
   def voice_conversion(self, y, y_lengths, sid_src, sid_tgt):
